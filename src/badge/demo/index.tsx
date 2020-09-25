52f8c870b1fa942a34badd9c58d0ad74f6d6c76b
qingzi.wang <qingzi.wang@funenc.com>
2020-09-25 11:01:35 +0800

    chore:样式及示例调整

--- src/badge/demo/index.tsx
+++ src/badge/demo/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Badge } from '../../index';
+import { Badge, List, Icon, Toast } from '../../index';
 import '../style/index.less';
+
+const Item = List.Item;
 
 const Demo = () => {
 
@@ -11,28 +13,15 @@ const Demo = () => {
         <div className="d-demo-nav__title">Badge</div>
       </div>
       <section className="d-demo-content">
-        <div className="d-demo-block">
-          <Badge dot>
-            <div style={{ width: '30px', height: '30px', background: '#ddd' }} />
-          </Badge>
-        </div>
-        <div className="d-demo-block">
-          <Badge text={77} overflowCount={55} />
-        </div>
-        <div className="d-demo-block">
-          <Badge text={'new'} />
-        </div>
-        <div className="d-demo-block">
-          <Badge text="减" />
-          <Badge text="惠" style={{ marginLeft: 12 }} />
-          <Badge text="免" style={{ marginLeft: 12 }} />
-          <Badge text="反" style={{ marginLeft: 12 }} />
-          <Badge text="hot" style={{ marginLeft: 12 }} />
-        </div>
-        <div className="d-demo-block">
-          <Badge text="券" style={{ padding: '0 3px', backgroundColor: '#f19736', borderRadius: 2 }} />
-          <Badge text="有机" style={{ marginLeft: 12, padding: '0 3px', backgroundColor: '#21b68a', borderRadius: 2 }} />
-          <Badge text="自动升级"
+
+        <List renderHeader={() => '基础样式'}>
+          <Item extra={<Badge dot><div style={{ width: '30px', height: '30px', background: '#ddd' }} /></Badge>}>实心微标</Item>
+          <Item extra={<Badge text={77} />} arrow="horizontal" onClick={() => Toast.info({ content: '这是列表链接', duration: 500 })}>数字</Item>
+          <Item extra={<Badge text={'new'} />}>文本1</Item>
+          <Item extra={<Badge text={'减'} />}>文本2</Item>
+          <Item extra={<Badge text="券" style={{ padding: '0 3px', backgroundColor: '#f19736', borderRadius: 2 }} />}>样式自定义1</Item>
+          <Item extra={<Badge text="有机" style={{ marginLeft: 12, padding: '0 3px', backgroundColor: '#21b68a', borderRadius: 2 }} />}>样式自定义2</Item>
+          <Item extra={<Badge text="自动升级"
             style={{
               marginLeft: 12,
               padding: '0 3px',
@@ -41,11 +30,12 @@ const Demo = () => {
               color: '#f19736',
               border: '1px solid #f19736',
             }}
-          />
-        </div>
+          />}>样式自定义3</Item>
+        </List>
+        <h2 className="d-demo-block__title">扩展</h2>
         <div className="d-demo-block">
           <Badge text={'促'} corner>
-            <div style={{width:120,height:50,lineHeight:'50px'}}>新产品上新啦</div>
+            <div style={{width:120,height:40,lineHeight:'40px'}}>新产品上新啦</div>
           </Badge>
         </div>
       </section>
